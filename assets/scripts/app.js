465c5b7c67afc2b679005a5de96c855d4fd16059
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-20 06:55:09 +0530

    Creating object of XMLHttpRequest and configuring a request with 'GET' Method

---
+++ assets/scripts/app.js
@@ -0,0 +1,4 @@
+// Creating an object of XMLHttpRequest
+const xhr = new XMLHttpRequest();
+// Configuring a request, open takes 2 arguments first 'method' and second 'url'
+xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
