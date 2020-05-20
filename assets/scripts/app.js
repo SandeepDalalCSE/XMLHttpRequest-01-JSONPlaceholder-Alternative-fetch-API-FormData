1215640432e3b201f845c9e931dfd9d2a05c997c
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-20 07:01:23 +0530

    sending a 'GET' request.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
 // Creating an object of XMLHttpRequest
 const xhr = new XMLHttpRequest();
-// Configuring a request, open takes 2 arguments first 'method' and second 'url'
+// Configuring a request, open takes 2 arguments first 'method' and second 'url'. Basically we have informed xhr which kind of request we want to send to address or to the end point.
 xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+// this will send the request. please check the network tab in developer tool options in the browser.
+xhr.send();
