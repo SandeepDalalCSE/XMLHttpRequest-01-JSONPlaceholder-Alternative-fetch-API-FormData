756b5f848032b09ff99226a37d7be9800bd2295f
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-20 07:25:55 +0530

    Converting JSON data to JavaScript data

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,11 @@ xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
 // this will send the request. please check the network tab in developer tool options in the browser.
 xhr.send();
 
+// xhr.responseType = "json"; // instead of using parse method, we can also define our response type initially.
+
 xhr.onload = function() {
   console.log(xhr); // Consoling complete object.
-  console.log(xhr.response); // Consoling only response key in xhr object. Note: response is in JSON (JavaScript Object Notation) format. To use this data we need to convert it first into string.
+  console.log(xhr.response); // Consoling only response key in xhr object. Note: response is in JSON (JavaScript Object Notation) format. To use this data we need to convert it first into JavaScript string/object.
+  const listOfPosts = JSON.parse(xhr.response); // Data parsing from JSON to JS Object using JSON.parse() helper method.
+  console.log(listOfPosts);
 };
