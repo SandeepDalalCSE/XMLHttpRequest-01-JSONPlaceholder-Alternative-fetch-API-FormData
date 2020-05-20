6ddf2a2e125490a6f0f942737cdecc4651b70244
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-20 07:13:52 +0530

    loading data in console.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,3 +4,8 @@ const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
 // this will send the request. please check the network tab in developer tool options in the browser.
 xhr.send();
+
+xhr.onload = function() {
+  console.log(xhr); // Consoling complete object.
+  console.log(xhr.response); // Consoling only response key in xhr object. Note: response is in JSON (JavaScript Object Notation) format. To use this data we need to convert it first into string.
+};
