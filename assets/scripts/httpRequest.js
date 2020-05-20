6fe99e16df5fe040d8ceb25db53a5f39a70f0ce6
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-20 15:48:48 +0530

    Implementing error handling

--- assets/scripts/httpRequest.js
+++ assets/scripts/httpRequest.js
@@ -12,13 +12,25 @@ function sendHttpRequest(method, url, data) {
     // Configuring a request, open takes 2 arguments first 'method' and second 'url'. Basically we have informed xhr which kind of request we want to send to address or to the end point.
     xhr.open(method, url);
     // this will send the request. please check the network tab in developer tool options in the browser.
+
     // xhr.send();
     xhr.send(JSON.stringify(data));
 
     // xhr.responseType = "json"; // instead of using parse method, we can also define our response type initially.
 
     xhr.onload = function() {
-      resolve(xhr.response);
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(new Error("something went wrong!"));
+      }
+    };
+
+    // Handling error
+    xhr.onerror = function() {
+      reject(new Error("Failed to send a request"));
+      console.log(xhr.response);
+      console.log(xhr.status);
     };
   });
   return promise;
@@ -40,14 +52,18 @@ function sendHttpRequest(method, url, data) {
 
 // an alternative to fetch post using async and await.
 async function fetchPosts() {
-  const respnseData = await sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts");
-  const listOfPosts = JSON.parse(respnseData); // Data parsing from JSON to JS Object using JSON.parse() helper method.
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body;
-    postEl.querySelector("li").id = post.id;
-    listElement.append(postEl);
+  try {
+    const respnseData = await sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/pos");
+    const listOfPosts = JSON.parse(respnseData); // Data parsing from JSON to JS Object using JSON.parse() helper method.
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector("h2").textContent = post.title.toUpperCase();
+      postEl.querySelector("p").textContent = post.body;
+      postEl.querySelector("li").id = post.id;
+      listElement.append(postEl);
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
 
